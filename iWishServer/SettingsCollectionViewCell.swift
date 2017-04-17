1e026ebd3cfce1f01e2cea533097e914c62e71a3
Julius <julius@Juliuss-Mac.local>
2017-04-17 08:35:47 -0700

    leider nix wirklich neu ausser sone klasse

---
+++ iWishServer/SettingsCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsCollectionViewCell.swift
+//  iWishServer
+//
+//  Created by Julius on 17/04/2017.
+//  Copyright © 2017 Julius. All rights reserved.
+//
+
+import UIKit
+
+class SettingsCollectionViewCell: UICollectionViewCell {
+    
+    
+}
